c49b587e9bc8aefe13673f221702a6b5a1282ed4
Al-Mclean <al_mclean@hotmail.com>
2023-01-12 00:03:44 +1030

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 
 import { API } from 'homebridge';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { PLATFORM_NAME } from './settings';
 import { ExampleHomebridgePlatform } from './platform';
 
@@ -8,7 +9,7 @@ import { ExampleHomebridgePlatform } from './platform';
  * This method registers the platform with Homebridge
  */
 export = (api: API) => {
-  api.registerPlatform(PLATFORM_NAME, ExampleHomebridgePlatform);
+  api.registerPlatform('homebridge-plugin-iammeter', ExampleHomebridgePlatform);
 };
 
 // console.log('IAMMeter output test - 1');
