48c248ba4a734d0b2f255a0c9df5ee2f3e410930
Al-Mclean <al_mclean@hotmail.com>
2023-01-12 22:41:43 +1030

    Update

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'IAMMeter';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-IAMMeter';
+export const PLUGIN_NAME = 'homebridge-iammeter2';
